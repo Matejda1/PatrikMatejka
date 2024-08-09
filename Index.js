7a30fe19a35c24a103353bdf6c938fb0798536ca
Matejda1 <matejka.patri@gmail.com>
2024-08-09 16:00:01 +0200

    Prvnístranka

---
+++ Index.js
@@ -0,0 +1,22 @@
+// let display = document.getElementById("display");
+// 
+// display.addEventListener("click", function(){ alert("Hello World!"); });
+// 
+// 
+// let povoleneSpecialniZnaky = ["+", "-"];
+// 
+// if (povoleneSpecialniZnaky.includes() || typeof char == "number") {
+// 
+// }
+// else {
+//     //
+// }
+// 
+// function a(event){
+//     console.log(event);
+// }
+
+
+function a(event) {
+    console.log(event);
+}
